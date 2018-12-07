174e1021e72673a4810e668b8ca5b8d2c8207118
Andres Morey <andresmarcel@gmail.com>
2018-12-07 17:21:04 -0400

    bugfix for IE Edge crossdomain css securityerror

--- examples/assets/loadjs/loadjs.js
+++ examples/assets/loadjs/loadjs.js
@@ -137,8 +137,8 @@ function loadFile(path, callbackFn, args, numTries) {
         if (!e.sheet.cssText.length) result = 'e';
       } catch (x) {
         // sheets objects created from load errors don't allow access to
-        // `cssText`
-        result = 'e';
+        // `cssText` (unless error is Code:18 SecurityError)
+        if (x.code != 18) result = 'e';
       }
     }
 
